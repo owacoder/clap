dfcad1bf20ddbbd7c5ee14207468d50662a3b8f2
owacoder <owacoder@gmail.com>
2019-07-05 18:40:18 -0400

    Create string.h

---
+++ string.h
@@ -0,0 +1,13 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include <stddef.h>
+#include "defines.h"
+
+#ifndef EOF
+#define EOF (-1)
+#endif
+
+/* TODO: implement header for string.c */
+
+#endif // STRING_H
