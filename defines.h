0961dcc3e0265243c0d8bff154bf26cfdcd93c77
owacoder <owacoder@gmail.com>
2019-07-05 18:39:49 -0400

    Create defines.h

---
+++ defines.h
@@ -0,0 +1,12 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+/* C Language Accessibility Platform */
+
+/* Define CLAP_DYNAMIC_MEMORY to enable dynamic memory management, otherwise all functions using it will be removed. */
+#define CLAP_DYNAMIC_MEMORY
+
+/* Define CLAP_SAVE_SPACE to enable very space-conscious versions of functions, rather than focusing on speed. */
+#define CLAP_SAVE_SPACE
+
+#endif // DEFINES_H
